b0af8f3548e8768c64bcf12c5cf95acf03c3488b
Ben <bgardner620@gmail.com>
2017-07-18 20:36:52 -0400

    new

--- src/theme.js
+++ src/theme.js
@@ -1,19 +1,17 @@
 
 const Color = require('color');
-
 const honeydew = '#E3F9EC';
 const lightGray = '#CDD5D1';
 const silverChalice = '#B4A6AB';
 const etonBlue = '#7FC6A4';
 const wenge = '#615055';
 const canvasColor = Color(lightGray).lighten(0.2).string();
-console.log(canvasColor);
 
 export default {
   fontFamily: 'Open Sans, sans-serif',
   palette: {
     primary1Color: silverChalice,
     textColor: wenge,
-    canvasColor,
+    canvasColor: canvasColor
   },
 };
